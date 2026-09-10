a7c90550d00f859b747f864f9cadd05c3d5b389b
Aryan <aryandubey@Aryans-Mac-mini.local>
2026-09-10 11:44:52 +0530

    chore: ignore pio_env and remove cached .pio directories from tracking

--- firmware/esp32_controller/.pio/build/esp32dev/lib37c/FastLED/platforms/esp/32/clockless_i2s_esp32s3.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib37c/FastLED/platforms/esp/32/clockless_i2s_esp32s3.cpp.o: \
- .pio/libdeps/esp32dev/FastLED/src/platforms/esp/32/clockless_i2s_esp32s3.cpp \
- /Users/aryandubey/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
- .pio/libdeps/esp32dev/FastLED/src/fl/has_include.h
